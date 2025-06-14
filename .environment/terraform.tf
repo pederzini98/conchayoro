ec83f3f018e45d31eec4e3a19d9f04dadcc6fcb5
Lucas Pederzini <48737620+pederzini98@users.noreply.github.com>
2025-06-14 14:46:04 -0300

    change action

--- .environment/terraform.tf
+++ .environment/terraform.tf
@@ -1,18 +1,10 @@
 terraform {
-    required_providers {
-        aws = {
-        source = "hashicorp/aws"
-        version = "~> 5.98"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.98"
     }
-    backend "s3" {
-        bucket = "${var.S3_BUCKET}"
-        key = "${secrets.AWS_ACCESS_KEY_ID}"
-        region = "${var.AWS_REGION}"
-        encrypt = true
-    }
-        required_version = ">= 1.12.1"
-}
-provider "aws" {
-    region = "${var.AWS_REGION}"
+  }
+
+  backend "s3" {}  # Variáveis serão passadas pela GitHub Action
 }
